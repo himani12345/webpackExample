5f3f2e680aa9a60b1a9a2464e5d7f1e4a1d338b0
himani12345 <hv2906@gmail.com>
2017-12-31 02:22:22 +0530

    added code splitting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import sum from './sum';
-import './image_viewer';
-const total =sum(10,3);
-console.log(total);
+const button = document.createElement('button');
+button.innerText = 'Click Me';
+button.onclick = ()=>{
+    //load imageViewer.js only after user clicks on button --performance reasons
+    //system.import is an async call
+    //system.import call creates bundle.js and 0.bundle.js--code splitting
+    System.import('./image_viewer').then((module)=>{
+        console.log(module);
+    })
+}
+
+document.body.appendChild(button);
